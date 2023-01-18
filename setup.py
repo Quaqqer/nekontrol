962b5dbdd079aa2d0ea798ef2a7c1a1c1ff1da0d
Empa <emanuel@empa.xyz>
2023-01-18 23:51:48 +0100

    Pin dependency versions

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setup(
     name="kattest",
     version="0.1.0",
     package_dir={"kattest": "lib"},
-    install_requires=["click", "termcolor", "requests", "appdirs", "natsort"],
+    install_requires=[
+        "click==8.1.3",
+        "termcolor==2.0.1",
+        "requests==2.28.1",
+        "appdirs==1.4.4",
+        "natsort==8.2.0",
+    ],
     entry_points={"console_scripts": ["kattest = kattest.interactive.cli:cli"]},
 )
