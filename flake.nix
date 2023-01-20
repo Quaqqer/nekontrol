85a85eb345cac52535e1411ecf45d01c9c2c563e
Empa <emanuel@empa.xyz>
2023-01-20 03:48:02 +0100

    Nix shell with live editing, poetry lock file

--- flake.nix
+++ flake.nix
@@ -1,51 +1,23 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "nixpkgs";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        inherit (poetry2nix.legacyPackages.${system})
+          mkPoetryApplication mkPoetryEnv;
+
         pkgs = nixpkgs.legacyPackages.${system};
 
-        pythonDeps = pyPkgs: [
-          pyPkgs.click
-          pyPkgs.termcolor
-          pyPkgs.requests
-          pyPkgs.appdirs
-          pyPkgs.natsort
-        ];
-
-        nekontrol = (pkgs.callPackage ({ python310, installShellFiles }:
-          python310.pkgs.buildPythonApplication rec {
-            pname = "nekontrol";
-            version = "0.1.4";
-
-            src = ./.;
-            format = "pyproject";
-
-            nativeBuildInputs = [ installShellFiles ];
-
-            propagatedBuildInputs = pythonDeps python310.pkgs ++ [python310.pkgs.poetry-core];
-            postInstall = ''
-              installShellCompletion --cmd nekontrol \
-                --bash <(_NEKONTROL_COMPLETE=bash_source $out/bin/nekontrol) \
-                --zsh <(_NEKONTROL_COMPLETE=zsh_source $out/bin/nekontrol) \
-                --fish <(_NEKONTROL_COMPLETE=fish_source $out/bin/nekontrol) \
-            '';
-          }) { });
-      in {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [ pkgs.bashInteractive ];
-          buildInputs = with pkgs; [
-            (python310.withPackages pythonDeps)
-
-            poetry
-
-            nekontrol
-          ];
+        poetryArgs = {
+          projectDir = self;
+          python = pkgs.python310;
         };
-
-        packages.default = nekontrol;
-        packages.nekontrol = nekontrol;
+      in {
+        packages.nekontrol = mkPoetryApplication poetryArgs;
+        packages.default = self.packages.${system}.nekontrol;
       });
 }
---
+++ shell.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  nekontrolEnv = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./.;
+    editablePackageSources = { nekontrol = ./src; };
+  };
+in nekontrolEnv.env.overrideAttrs (oldAttrs: { buildInputs = [ ]; })
