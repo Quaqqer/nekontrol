2d59e609a236d3f31d3afa1f26a05e765a599a6d
Empa <emanuel@empa.xyz>
2023-02-09 22:46:44 +0100

    Update demo again

--- res/demo.sh
+++ res/demo.sh
@@ -2,7 +2,7 @@
 
 # Run with:
 # nix shell nixos#doitlive nixos#asciinema nixos#pypy3
-# (cd examples && asciinema rec --cols 100 --rows 25 -c 'doitlive play ../demo.sh -q' ../demo.cast && cd .. && svg-term --in demo.cast --out demo.svg --window --padding-x 1)
+# (cd examples && asciinema rec --overwrite --cols 100 --rows 25 -c 'doitlive play ../demo.sh -q' ../demo.cast && cd .. && svg-term --in demo.cast --out demo.svg --window --padding-x 1)
 
 #doitlive commentecho: true
 #doitlive alias: ls=exa
@@ -18,3 +18,5 @@ nk incorrect.py
 nk error.py
 
 nk twostones.py
+
+nk compile_error.cc
