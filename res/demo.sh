eb2050534898ca1c11f21ef4a6c3634603ce5d8e
Empa <emanuel@empa.xyz>
2023-02-09 22:34:29 +0100

    Update demo

--- res/demo.sh
+++ res/demo.sh
@@ -2,7 +2,7 @@
 
 # Run with:
 # nix shell nixos#doitlive nixos#asciinema nixos#pypy3
-# (cd examples && asciinema rec -c 'doitlive play ../demo.sh -q' ../demo.cast && cd .. && svg-term --in demo.cast --oout demo.svg --window --padding-x 1)
+# (cd examples && asciinema rec --cols 100 --rows 25 -c 'doitlive play ../demo.sh -q' ../demo.cast && cd .. && svg-term --in demo.cast --out demo.svg --window --padding-x 1)
 
 #doitlive commentecho: true
 #doitlive alias: ls=exa
